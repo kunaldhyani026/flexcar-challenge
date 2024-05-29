276126890c9c16ac977e47d1ef72701a0d44702b
Kunal <kunaldhyani026@gmail.com>
2024-05-29 12:05:15 +0530

    code comments added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,20 @@ class ApplicationController < ActionController::API
 
   private
 
+  # authenticates user before every controller action
+  # For simplicity, currently only check is to see if user_id is present in payload/headers or not.
+  # In production systems, user_id should be decoded from the auth token and
+  # then user_id should be used ahead for maintaining user's cart states.
   def authenticate_user
     render_error('unauthorized_user', 'invalid user_id ', 401) unless params[:user_id].present?
   end
 
+  # Method to render_error to be used across different controllers
   def render_error(type, message, http_status_code)
     render json: { error: { type:, message:} }, status: http_status_code
   end
 
+  # Method to render_response to be used across different controllers
   def render_response(response)
     render json: response['body'], status: response['status']
   end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -20,12 +20,14 @@ class CartsController < ApplicationController
 
   private
 
+  # Validates input params for add and remove action, returns error to user for bad input
   def validate_params
     unless params[:quantity].present? && params[:quantity].instance_of?(Integer) && params[:quantity].positive?
       render_error('invalid_request_error', 'Quantity should be a positive integer', 400)
     end
   end
 
+  # Finds the item from inventory before add or remove action, returns error to user if item doesn't exists
   def fetch_item
     @item = Item.find(params[:item_id])
   rescue ActiveRecord::RecordNotFound => e
